ae94fba477eedb0dae7a88d80da15557117e7f67
narpat-curl <85334304+narpat-curl@users.noreply.github.com>
2025-05-02 16:14:49 +0530

    minor change

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // This ensures assets are served from the root
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
-      external: ['react-router-dom']
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
     }
   },
   preview: {
